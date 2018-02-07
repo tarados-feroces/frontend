b500ab4a7dbbb3d81952cfa3ea8003cc266909e5
anton <kaabchok890098@yandex.ru>
2018-02-07 13:21:04 +0300

    added errors

--- sources/static/js/login.js
+++ sources/static/js/login.js
@@ -5,9 +5,11 @@ let regToValidateLogin = /^([a-z0-9_])+$/i;
 
 document.getElementsByClassName("loginButton")[0].addEventListener("click", () => {
     let inputs = [document.getElementsByClassName("loginLoginInput")[0], document.getElementsByClassName("passwordLoginInput")[0]];
+    let errors = document.getElementsByClassName("error");
 
-    alert(inputs[0].value.search(regToValidateLogin) !== -1 ? "ok" : "wrong login");
-    alert(inputs[1].value.search(regToValidateLogin) !== -1 ? "ok" : "wrong password");
+    inputs[0].value.search(regToValidateLogin) === -1 ? errors[0].style.display = "block" : errors[0].style.display = "none";
+
+    inputs[1].value.search(regToValidateLogin) === -1 ? errors[1].style.display = "block" : errors[1].style.display = "none";
 
     if (inputs[0].value.search(regToValidateLogin) !== -1 && inputs[1].value.search(regToValidateLogin) !== -1) {
         document.location.href = 'authorized.html';
