70cb995ce15289c5d65078f4e028fc834c866925
kabachok169 <kabachok890098@yandex.ru>
2018-02-07 21:50:10 +0300

    changed structure

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ let app = express();
 
 let path = require('path');
 
-app.use('', express.static(path.join('sources')));
+app.use('', express.static(path.join('../src')));
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
