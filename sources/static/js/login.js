01a1ae890e34922a60d252b7411800d3405d3396
anton <kaabchok890098@yandex.ru>
2018-02-07 11:27:28 +0300

    added redirect & validation

--- sources/static/js/login.js
+++ sources/static/js/login.js
@@ -1,3 +1,17 @@
 'use strict';
 
-document.getElementsByClassName("loginButton")[0].addEventListener("click", () => alert("lol"));
+
+let regToValidateLogin = /^([a-z0-9_])+$/i;
+
+document.getElementsByClassName("loginButton")[0].addEventListener("click", () => {
+    let inputs = [document.getElementsByClassName("loginLoginInput")[0], document.getElementsByClassName("passwordLoginInput")[0]];
+
+    alert(inputs[0].value.search(regToValidateLogin) !== -1 ? "ok" : "wrong login");
+    alert(inputs[1].value.search(regToValidateLogin) !== -1 ? "ok" : "wrong password");
+
+    if (inputs[0].value.search(regToValidateLogin) !== -1 && inputs[1].value.search(regToValidateLogin) !== -1) {
+        document.location.href = 'authorized.html';
+    }
+});
+
+
