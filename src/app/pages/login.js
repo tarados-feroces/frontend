7f8158efd03bb0ff38b2a6ce6601b1ac3febb377
kabachok169 <kabachok890098@yandex.ru>
2018-02-09 18:29:06 +0300

    added linter

--- src/app/pages/login.js
+++ src/app/pages/login.js
@@ -3,7 +3,8 @@
 
 const regToValidateLogin = /^([a-z0-9_])+$/i;
 
-const inputs = [document.getElementsByClassName("loginLoginInput")[0], document.getElementsByClassName("passwordLoginInput")[0]];
+const inputs = [...document.getElementsByClassName('login__input')];
+const inputs_blocks = [...document.getElementsByClassName('login-block__input-block')];
 const errors = document.getElementsByClassName("error");
 
 document.getElementsByClassName("loginButton")[0].addEventListener("click", () => {
@@ -12,14 +13,19 @@ document.getElementsByClassName("loginButton")[0].addEventListener("click", () =
     }
 });
 
-inputs.forEach((item, i) => {
-    item.onblur = () => {
-        item.value.search(regToValidateLogin) === -1 ? errors[i].style.display = "block" : errors[i].style.display = "";
-    };
+const validation = (input) => {
+    console.log(input.getElementsByTagName("input")[0]);
+    input.getElementsByTagName("input")[0].addEventListener('blur', () => {
+        input.getElementsByTagName("input")[0].value.search(regToValidateLogin) === -1 ? input.getElementsByTagName("error")[0].style.display = "block" : input.getElementsByTagName("error")[0].style.display = "";
+    });
 
-    item.onfocus = () => {
-        if (errors[i].style.display === "block") {
-            errors[i].style.display = "";
+    input.getElementsByTagName("input")[0].onfocus = () => {
+        if (input.getElementsByTagName("error")[0].style.display === "block") {
+            input.getElementsByTagName("error")[0].style.display = "";
         }
     };
+};
+
+inputs_blocks.forEach((item) => {
+    validation(item);
 });
