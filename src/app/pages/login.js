c5c209a93bf83759a7e8abd69f6a4fb24a33f382
kabachok169 <kabachok890098@yandex.ru>
2018-02-09 23:44:45 +0300

    simply style fix

--- src/app/pages/login.js
+++ src/app/pages/login.js
@@ -1,9 +1,8 @@
 'use strict';
 
-
 const regToValidateLogin = /^([a-z0-9_])+$/i;
 
-const inputs_blocks = [...document.getElementsByClassName('login-block__input-block')];
+const inputsBlocks = [...document.getElementsByClassName('login-block__input-block')];
 
 document.getElementsByClassName("login-block__login-button")[0].addEventListener("click", () => {
     if (inputs[0].value.search(regToValidateLogin) !== -1 && inputs[1].value.search(regToValidateLogin) !== -1) {
@@ -13,8 +12,8 @@ document.getElementsByClassName("login-block__login-button")[0].addEventListener
 
 const validation = inputBlock => {
 
-    let input = inputBlock.getElementsByTagName("input")[0];
-    let error = inputBlock.getElementsByClassName("error")[0];
+    const input = inputBlock.getElementsByTagName("input")[0];
+    const error = inputBlock.getElementsByClassName("error")[0];
 
     input.addEventListener('blur', () => {
         input.value.search(regToValidateLogin) === -1 ? error.style.display = "block" : error.style.display = "";
@@ -27,6 +26,4 @@ const validation = inputBlock => {
     });
 };
 
-inputs_blocks.forEach((item) => {
-    validation(item);
-});
+inputsBlocks.forEach(item => validation(item));
