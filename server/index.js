9060c4c5778c7773ad94a27c239c05f905bf24c4
Danchetto <danchetto@yandex.ru>
2018-02-09 22:55:16 +0300

    + template test

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ let app = express();
 let path = require('path');
 
 app.use('', express.static(path.join('src')));
+app.use('', express.static(path.join('node_modules')));
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
