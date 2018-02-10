6361912d56d5e2192610c70b1c06c62182656dca
kabachok169 <kabachok890098@yandex.ru>
2018-02-10 15:06:36 +0300

    try to do templates

--- src/app/pages/test.js
+++ src/app/pages/test.js
@@ -1,13 +1,9 @@
 'use strict';
 
-
 const source = document.getElementById("entry-template").innerHTML;
 const template = Handlebars.compile(source);
 
 const context = {title : "Hello world!"};
 const html = template(context);
 
-const title = document.createElement('div');
-title.innerHTML = html;
-
-document.body.appendChild(title);
+document.write(html);
